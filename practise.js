72b476c9e0dbf055aaf1daffe75be13cf1208774
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-12-20 10:26:53 +0500

    Add files via upload

---
+++ practise.js
@@ -0,0 +1,10 @@
+//  practise question create a new element . give it is a text " click me"background color of red and text color of white
+//  insert the button at the first element inside the body tag
+// let newBtn=document.createElement("button");
+// newBtn.innerText="click me!";
+// newBtn.style.color="white";
+// newBtn.style.backgroundColor="red";
+// document.querySelector("body").prepend(newBtn);
+//  create a <p> tag in html , gives it a class and some styling . Now create a new class in Css and try to append this to the <p> element.
+//  Did you notice , how you overwrite the class name when you add a new one? solve this problem using classList
+let para=document.querySelector("p");
